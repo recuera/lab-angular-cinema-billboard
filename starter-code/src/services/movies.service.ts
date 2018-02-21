feee0be660727c48cc4f9b4bad9820c943e80da9
Ana Recuero <anarecsa@gmail.com>
2018-02-21 17:11:32 +0100

    movies service created

---
+++ starter-code/src/services/movies.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from "@angular/core";
+
+export interface Movie {
+  id:number,
+  title:string,
+  poster:string,
+  synopsis:string,
+  genres:Array<string>,
+  year:number,
+  director:string,
+  actors:Array<string>,
+  hours:Array<string>,
+  room:number
+}
+
+@Injectable()
+export class moviesService{
+  movies:Array<Movie>
+
+  getMovies(){
+    return this.movies;
+  }
+  getMovie(id){
+    this.movies.forEach(m => {
+      if(m.id = id){
+        return m;
+      }
+    })
+    console.log(`No movie found with id: ${id}`)
+  }
+}
